3f8e4b5e6b56da4f934be66db854df26f596eb7b
Andy Little <kwikius@yahoo.com>
2019-11-28 19:39:03 +0000

    use quan compatibility header

--- example/trilateration.cpp
+++ example/trilateration.cpp
@@ -12,88 +12,24 @@
   requires my quan library ( headers only required)
   https://github.com/kwikius/quan-trunk
 */
-
 #include <iostream>
+
 
 #include <units/dimensions/length.h>
 #include <units/math.h>
 
-#include <quan/two_d/out/vect.hpp>
+#include <quan/compatibility/mpusz_units.hpp>
 #include <quan/three_d/out/vect.hpp>
-#include <quan/meta/binary_op.hpp>
-#include <quan/meta/is_scalar.hpp>
-
-
-namespace quan{ namespace meta{ namespace impl{
-
-   /*
-      quan has a different view on what a scalar is to mpusz::units.
-      No matter, though confusing, they can work together
-   */
-   template <units::Quantity Q>
-   struct is_scalar_impl<Q> : std::true_type{};
-
-   // quan (has been around since 2003) uses specialisations of binary_op_impl
-   // for result type deduction on operations. Back then there was no such thing as decltype and auto etc.
-   // Though you can use decltype for deduction, there may be some types
-   // that dont have any runtime operations so this type may still be useful
-   // though maybe one could define the runtime operators on the meta types anyway
-   // anyway here is the basic integration to get units::Quantity working with quan::two_d::vector
-
-   template <units::Quantity Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::plus, Rhs>{
-      typedef decltype(Lhs{} + Rhs{}) type;
-   };
-
-   template <units::Quantity Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::minus, Rhs>{
-      typedef decltype(Lhs{} - Rhs{}) type;
-   };
-
-   template <units::Quantity Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::times, Rhs>{
-      typedef decltype(Lhs{} * Rhs{}) type;
-   };
-
-   template <units::Quantity Lhs, units::Scalar Rhs>
-   struct binary_op_impl<Lhs, quan::meta::times, Rhs>{
-      typedef decltype(Lhs{} * Rhs{}) type;
-   };
-
-   template <units::Scalar Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::times, Rhs>{
-      typedef decltype(Lhs{} * Rhs{}) type;
-   };
-
-   template <units::Quantity Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::divides, Rhs>{
-      typedef decltype(Lhs{} / Rhs{}) type;
-   };
-
-   template <units::Quantity Lhs, units::Scalar Rhs>
-   struct binary_op_impl<Lhs, quan::meta::divides, Rhs>{
-      typedef decltype(Lhs{} / Rhs{}) type;
-   };
-
-   template <units::Scalar Lhs, units::Quantity Rhs>
-   struct binary_op_impl<Lhs, quan::meta::divides, Rhs>{
-      typedef decltype(Lhs{} / Rhs{}) type;
-   };
-
-}}}
 
 using namespace units;
 namespace {
 
-  //N.B at some point use affine quantity type but for now this has to do.
-  typedef quan::three_d::vect<quantity<kilometre> > point;
+   typedef quan::three_d::vect<quantity<kilometre> > point;
 
-  typedef quan::three_d::vect<quantity<kilometre> > point3D;
-
-  struct sphere{
-     point centre;
-     quantity<kilometre> radius;
-  };
+   struct sphere{
+      point centre;
+      quantity<kilometre> radius;
+   };
 
    std::ostream & operator<< ( std::ostream & out, sphere const & c)
    {
@@ -103,7 +39,7 @@ namespace {
 
 int main()
 {
-   // TODO matric pre-calc so that spheres can be arbitraily positioned in 3d
+   // TODO matrix pre-calc so that spheres can be arbitraily positioned in 3d
 
    sphere A{{0.0km,0.0km,0.0km},8.0km};
    //sphere A must be centred at origin
@@ -146,7 +82,7 @@ int main()
    if ( z_2 >= pow<2>(0km)){
       auto const z = sqrt(z_2);
 
-      point3D intersection_point {x,y,z};
+      point intersection_point {x,y,z};
 
       std::cout << "intersection point = " << intersection_point << '\n';
      
